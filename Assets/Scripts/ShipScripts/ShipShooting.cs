e0d9f1183d6034aacaec98c66c5c014c9b49bcea
Justin Piantedosi <jpiante@aol.com>
2016-02-20 13:27:05 -0600

    Useless information and commit2

--- Assets/Scripts/ShipScripts/ShipShooting.cs
+++ Assets/Scripts/ShipScripts/ShipShooting.cs
@@ -4,16 +4,37 @@ using System.Collections;
 public class ShipShooting : MonoBehaviour
 {
     public MouseClickedMe clickedScript;
+    public GameObject shell = GameObject.Find("Shell");
+    int power;
+    public float x_dest;
+    public float z_dest;
+    public float y_height = 8.19F;
+    Vector3 vector = new Vector3(x, , camera.ScreenToWorldPoint());
+    Camera maincam;
+
+
+    // get mouse position
+    Ray getMouseLocation()
+    {
+        maincam = Camera.main;
+        Ray ret = maincam.ScreenPointToRay(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0));
+
+        return ret;
+    }
+
 
     void Start ()
     {
-	
-	}
-	
+        shell.SetActive(true); // activate shell
 
-	void Update ()
+    }
+
+
+    void Update ()
     {
-        if(clickedScript.shipEnabled == true) // if the ship is selected
+
+
+        if (clickedScript.shipEnabled == true) // if the ship is selected
         {
 
         }
