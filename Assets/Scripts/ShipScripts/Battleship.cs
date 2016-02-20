89b39939f722e144cc0f8cd5cd7b702f12eb10a0
Wynn Drahorad <wynnd5595@gmail.com>
2016-02-20 16:05:48 -0600

    Added Ship base class and Battleship subclass, and Cruiser and Battleship prefabs

---
+++ Assets/Scripts/ShipScripts/Battleship.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Battleship : Ship {
+
+    Battleship (int x, int z)
+    {
+        x_pos = x;
+        z_pos = z;
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        // init ship stats
+        health = 10;
+        power = 4;
+        speed = 2;
+
+        // build and position ship object
+        prefab_location = "/Prefabs/Battleship";
+        the_ship = (GameObject)Resources.Load(prefab_location);
+        the_ship.transform.position = new Vector3(x_pos, water_height-1, z_pos);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/Scripts/ShipScripts/Ship.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ship : MonoBehaviour {
+
+
+    public float water_height = 8.19F;
+    protected int health, speed, power, x_pos, z_pos;
+    protected GameObject the_ship;
+    protected string prefab_location;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
