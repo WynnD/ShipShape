25d3d998d38d55f68eda736b43aded24569e409c
Justin Piantedosi <jpiante@aol.com>
2016-02-28 17:34:54 -0600

    Created LevelManager script still wip

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -6,17 +6,51 @@ public class LevelManager : MonoBehaviour
     //KEEPS TRACK OF RANDOM GOINGS ON IN THE LEVEL
 
     public bool p1Turn;
+    public GameObject[] p1Ships; //active p1 ships in the scene
+    public GameObject[] p2Ships; //active p2 ships in the scene
 
 
 
 	void Start ()
     {
         p1Turn = true;
-	}
+        p1Ships = GameObject.FindGameObjectsWithTag("P1Ship");
+        p2Ships = GameObject.FindGameObjectsWithTag("P2Ship");
+    }
 	
 
 	void Update ()
     {
-	
-	}
+        //Turn System
+	    if(p1Turn == true)
+        {
+            //do all the things that are necessary for p1 to be active.
+            //Debug.Log("P1 Turn");
+        }
+        else
+        {
+            //do all the things that are necessary for p2 to be active.
+            //Debug.Log("P2 Turn");
+        }
+
+
+
+
+
+
+
+
+
+        //Winning or Losing
+        if (p1Ships.Length <= 0)
+        {
+            //P1 lost the game.
+            Debug.Log("P1 Lost the Game.");
+        }
+        if (p1Ships.Length <= 0)
+        {
+            //P1 lost the game.
+            Debug.Log("P2 Lost the Game.");
+        }
+    }
 }
